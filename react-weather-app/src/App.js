168cad8a93a93f9c766ddc16c59144369883f532
zbin66666 <binz6@uci.edu>
2023-10-03 14:49:19 -0700

    fiexed typo

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import countries from 'i18n-iso-countries';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTemperatureLow, faTermperatureHigh, faMapMarkerAlt, faTemperatureHigh } from "@fortawesome/free-solid-svg-icons";
+import { faTemperatureLow, faTemperatureHigh, faMapMarkerAlt, } from "@fortawesome/free-solid-svg-icons";
 
 countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
 
@@ -11,19 +11,18 @@ function App() {
   //State
   const [apiData, setApiData] = useState({});
   const [getState, setGetState] = useState('Irvine, USA');
-  const [state,setState]= useState ('Irvine, USA');
+  const [state, setState] = useState ('Irvine, USA');
 
   //API KEY AND URL
-  const apiKey = process
-  .env.REACT_APP_API_KEY;
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?1=${state}&${apiKey}`;
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
   
   //Side Effect
   useEffect(() => {
     fetch(apiUrl)
     .then((res) => res.json())
     .then((data) => setApiData(data));
-  }, [apiUrl]);
+  },[apiUrl]);
 
   const inputHandler = (event) => {
     setGetState(event.target.value);
@@ -32,7 +31,7 @@ function App() {
     setState(getState);
   };
   const kelvinToCelsius = (k) => {
-    return (k-273.15).toFixed(0) 
+    return (k-273.15).toFixed(1);
   }
   return (
     <div className="App">
@@ -62,65 +61,66 @@ function App() {
               <div className="card mt-3 mx-auto">
                 {/*Is it ture data coming in from open weather based on input location*/}
                 {apiData.main
-                  ? (<div class="card-body text-center"> 
-                    <img 
-                      src={`http://openweathermap.org/img/w${apiData.weather[0].icon}.png`}
-                      alt="weather status icon"
-                      className="weather-icon"></img>
-                      <p className='h2'>
+                  ?(<div class="card-body text-center"> 
+                      <img 
+                        src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
+                        alt="weather status icon"
+                        className="weather-icon"
+                      />
+                      <p className="h2">
                         {kelvinToCelsius(apiData.main.temp)}&deg; C
                       </p>
                       <p className="h5">
                         <FontAwesomeIcon
                         icon={faMapMarkerAlt}
-                        className="fas fa-1x mr-2 text-dark"/>{' '}
+                        className="fas fa-1x mr-2 text-dark"
+                        />{' '}
                         <strong>{apiData.name}</strong>
                       </p>
-                      <div className="row mt-4">
-                        <div className='col-md-6'>
-                          <p>
-                            <FontAwesomeIcon
-                              icon={faTemperatureLow}
-                              className="fas fa-1x mr-2 text-primary"/>{' '}
-                              <strong>
-                                {kelvinToCelsius(apiData.main.temp_min)}&deg; C
-                              </strong>
-                          </p>
-                          <p>
-                            <FontAwesomeIcon
-                              icon={faTemperatureHigh}
-                              className="fas fa-1x mr-2 text-danger"/>{' '}
-                              <strong>
-                                {kelvinToCelsius(apiData.main.temp_max)}&deg; C
-                              </strong>
-                          </p>
-                        </div>
-                        <div className='col-md-6'>
-                          <p>
-                            {' '}
-                            <strong>{apiData.weather[0].main}</strong>
-                          </p>
-                          <p>
+                    <div className="row mt-4">
+                      <div className="col-md-6">
+                        <p>
+                          <FontAwesomeIcon
+                            icon={faTemperatureLow}
+                            className="fas fa-1x mr-2 text-primary"
+                            />{' '}
                             <strong>
-                              {' '}
-                              {countries.getName(apiData.sys.country, 'en', {
-                                select: 'official'
-                              })}
+                              {kelvinToCelsius(apiData.main.temp_min)}&deg; C
                             </strong>
-                          </p>
-                        </div>
+                        </p>
+                        <p>
+                          <FontAwesomeIcon
+                            icon={faTemperatureHigh}
+                            className="fas fa-1x mr-2 text-danger"/>{' '}
+                            <strong>
+                              {kelvinToCelsius(apiData.main.temp_max)}&deg; C
+                            </strong>
+                        </p>
                       </div>
-                    </div>) /*load data if true */
-                  :(<h1>Loading</h1>
-                  ) /* Outmeassage if false */
-                }
+                      <div className="col-md-6">
+                        <p>
+                          {' '}
+                          <strong>{apiData.weather[0].main}</strong>
+                        </p>
+                        <p>
+                          <strong>
+                            {' '}
+                            {countries.getName(apiData.sys.country, 'en', {
+                              select: 'official',
+                            })}
+                          </strong>
+                        </p>
+                      </div>
+                    </div>
+                  </div>) 
+                  : (<h1>Loading...</h1>)}
               </div>
             </div>
           </div>
         </div>
       </div>
       <footer className="footer">
-        &copy; React Weather App by Bin, only for educational use
+        &copy; React Weather App By Bin, <br/> only for education
       </footer>
     </div>
   )
